f49eda07c42e2fc89f9ea51fe56650a4e22b2b34
Cooper <cooperlynn545@gmail.com>
2024-11-19 18:46:44 +0000

    work on creating base player class and other player classes

--- src/Userplayer.hpp
+++ src/Userplayer.hpp
@@ -9,12 +9,14 @@
 class UserPlayer:public Player{
 	private:
 		std::string name;
-        int chips;
+        int chips{};
 	public:
-        Userplayer(std::string name, int chips);
-        std::string getName() override;
-        int getChips() override;
-        void setChips(int chips);
+        UserPlayer(std::string name, int chips);
+        std::string getName () const ;
+        int getChips() const;
+		void changeChips(int newChips);
+		void incWins() ;
+		int getWins() const;
   }
 ;
 
