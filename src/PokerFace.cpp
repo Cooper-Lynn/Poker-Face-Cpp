54194ca98b41ae466518c69b3ddaddfa8a696085
Cooper <cooperlynn545@gmail.com>
2025-03-24 19:35:48 +0000

    Started training the AI

--- src/PokerFace.cpp
+++ src/PokerFace.cpp
@@ -186,8 +186,8 @@ void PokerFace::passGameRunner(std::unique_ptr<GameRunner>&gameRunner) {
 
 
 int main(int argc, char *argv[]) {
-    AIModel model(0.70 ,0.94 ,1);
-    model.quickTrain(50000);
+    AIModel model(0.09 ,0.94 ,1);
+    model.quickTrain(500000);
     PokerFace poker_face;
     MainGui gui(argc, argv, poker_face);
     return 0;
